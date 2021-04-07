c68a5783e51fb95c36b314474c2ed6ecee93fd64
Rodolfo Pelinson <rodolfopelinson@gmail.com>
2021-04-07 16:04:45 -0300

    Initial commit

---
+++ R/VIF_selection_function.R
@@ -0,0 +1,25 @@
+#' @title Automating the exclusion of multicolinear variables
+#'
+#' @description This function takes a matrix of variables and excludes those who exhibit
+#' a variance inflation factor higher than 3 for giving matrix of response variables. It uses the function vif.cca() from package vegan.
+#' @param Y A matrix of response variables
+#' @param X A matrix of predictor variables from which multicolinear variables will be excluded.
+
+
+VIF_selection <- function(Y,X){
+  New_X <- X
+  for(i in 1:length(colnames(X))){
+    XVIF <- vif.cca(rda(Y,New_X))
+    delete <-NA
+    higher_vif <- max(XVIF)
+
+    if(higher_vif>3){
+      delete <- match(higher_vif, XVIF)
+    }
+
+    if(is.na(delete)){break}else{
+      New_X <- New_X[,-delete]
+    }
+  }
+  return(New_X)
+}
